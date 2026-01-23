de9d05d7636af2fca2df88ccc448bc7599442685
NinJaWay <130268824+heeelol@users.noreply.github.com>
2026-01-23 15:20:56 +0800

    Rename Duke to Yoru and implement greeting for level 0

---
+++ src/main/java/Yoru.java
@@ -0,0 +1,11 @@
+public class Yoru {
+    public static void main(String[] args) {
+        String line = "_______________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Yoru");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
